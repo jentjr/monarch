8ac1a0ec9fce715078bf987779e1528fcd7f69bc
Justin Jent <jentjr@gmail.com>
2017-03-15 18:36:31 -0400

    use ggplot geom_sf and RColorBrewer

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(sf)
+library(ggplot2)
 
 counties <- st_read(dsn = "data", 
                     layer = "monarch_data_county_summaries_090915"
@@ -9,8 +10,16 @@ shinyServer(function(input, output, session) {
   
   output$map <- renderPlot({
    
+    palette <- switch(
+      input$divPalette, 
+      
+    )
+    
     layerSelected <- input$layerInput
-    plot(counties[[layerSelected]])
+    ggplot(counties) + geom_sf(aes_string(fill = layerSelected)) + 
+      theme_bw() + 
+      scale_fill_distiller(type = input$paletteType, 
+                           palette = input$palette)
   
    })
 
--- ui.R
+++ ui.R
@@ -1,8 +1,13 @@
 library(sf)
+library(ggplot2)
+library(RColorBrewer)
 
 counties <- st_read(dsn = "data", 
                     layer = "monarch_data_county_summaries_090915"
 )
+
+# remove GEOID, NAME, STATE_NAME, FIPS, 
+fill_vars <- names(counties)[-c(1:4)]
 
 shinyUI(fluidPage(
 
@@ -12,7 +17,27 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput("layerInput", "Select Layer to Display", names(counties))
+      selectInput("layerInput", "Select Layer to Display", fill_vars),
+      radioButtons("paletteType", "Select Palette", c("div", "qual", "seq")),
+      conditionalPanel(
+        condition = "input.paletteType == 'div'",
+        selectInput("divPalette", "Select Pallete",
+                    c("BrBG", "PiYG", "PRGn", "PuOr", "RdBu", "RdGy", 
+                      "RdYlBu", "RdYlGn", "Spectral"))
+      ),
+      conditionalPanel(
+        condition = "input.paletteType == 'qual'",
+        selectInput("qualPalette", "Select Palette", 
+                    c("Accent", "Dark2", "Paired", "Pastel1", 
+                      "Pastel2", "Set1", "Set2", "Set3"))
+      ),
+      conditionalPanel(
+        condition = "input.paletteType == 'seq",
+        selectInput("seqPalette", "Select Pallete",
+                    c("Blues", "BuGn", "BuPu", "GnBu", "Greens", "Greys", 
+                      "Oranges", "OrRd", "PuBu", "PuBuGn", "PuRd", "Purples", 
+                      "RdPu", "Reds", "YlGn", "YlGnBu", "YlOrBr", "YlOrRd")) 
+      )
     ),
 
     # Show a plot of the counties
